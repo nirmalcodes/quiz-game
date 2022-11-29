a7be945f1346cce79a370bca586222ae1b4a0a7a
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2022-11-29 15:09:30 +0530

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { images } from "./constants";
+import Home from "./pages/Home/Home";
+import GettingStart from "./pages/GettingStart/GettingStart";
 
 function App() {
   return (
-    <div className="App">
-      heelo there
+    <div
+      className="app"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(43, 19, 93, 0.75), rgba(43, 43, 43, 0.75)), url(${images.back01})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<GettingStart />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,112 @@
+const quiz = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answers: [
+      {
+        text: "New York",
+        correct: false,
+      },
+      {
+        text: "London",
+        correct: false,
+      },
+      {
+        text: "Paris",
+        correct: true,
+      },
+      {
+        text: "Dublin",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Who is CEO of Tesla?",
+    answers: [
+      {
+        text: "Bill Gates",
+        correct: false,
+      },
+      {
+        text: "Tony Stark",
+        correct: false,
+      },
+      {
+        text: "Elon Musk",
+        correct: true,
+      },
+      {
+        text: "Jeff Bezos",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "The iPhone was created by which company?",
+    answers: [
+      {
+        text: "Intel",
+        correct: false,
+      },
+      {
+        text: "Apple",
+        correct: true,
+      },
+      {
+        text: "Amazon",
+        correct: false,
+      },
+      {
+        text: "Microsoft",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "How many Harry Potter books are there?",
+    answers: [
+      {
+        text: "4",
+        correct: false,
+      },
+      {
+        text: "6",
+        correct: false,
+      },
+      {
+        text: "7",
+        correct: true,
+      },
+      {
+        text: "8",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "When did the website `Facebook` launch?",
+    answers: [
+      {
+        text: "2004",
+        correct: true,
+      },
+      {
+        text: "2005",
+        correct: false,
+      },
+      {
+        text: "2006",
+        correct: false,
+      },
+      {
+        text: "2007",
+        correct: false,
+      },
+    ],
+  },
+];
