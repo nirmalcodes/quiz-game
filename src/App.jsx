235abb51d56616b8c6f56f0c078e120c73dd950d
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-04-25 09:01:37 +0530

    Update and renew app with vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h2 className="text-teal-500">Hello world</h2>
+        </>
+    );
+}
+
+export default App;
