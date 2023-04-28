ad132cb40876bce18c849683d6d678891d7088a2
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-04-28 18:23:12 +0530

    Add basic layout to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { useEffect, useState } from 'react';
 
 function App() {
-    const [count, setCount] = useState(0);
+
+    function setPageTitle(title) {
+        document.title = title;
+    }
+
+    useEffect(() => {
+        setPageTitle('My Page Titles');
+    }, []);
 
     return (
         <>
-            <h2 className="text-teal-500">Hello world</h2>
+            <div className="grid h-screen w-full bg-teal-700">
+                <div className="mx-auto self-end bg-stone-500 px-6 py-3 lg:w-3/4">
+                    <div className="mb-4 rounded-full bg-green-600 px-4 py-2 text-center">
+                        quiz
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="rounded-full bg-yellow-600 px-4 py-2">
+                            A
+                        </div>
+                        <div className="rounded-full bg-yellow-600 px-4 py-2">
+                            B
+                        </div>
+                        <div className="rounded-full bg-yellow-600 px-4 py-2">
+                            C
+                        </div>
+                        <div className="rounded-full bg-yellow-600 px-4 py-2">
+                            D
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
